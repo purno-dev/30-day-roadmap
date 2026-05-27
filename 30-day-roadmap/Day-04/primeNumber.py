abca039e8fe27cc0a16145c330722ced57036c7f
Purno <purnorunjunshqul1@gmail.com>
2026-05-28 01:23:09 +0600

    Prime Number

--- 30-day-roadmap/Day-04/primeNumber.py
+++ 30-day-roadmap/Day-04/primeNumber.py
@@ -1,17 +1,20 @@
-
+isPrime =True
 number = int(input("Enter Number to Check if it's Prime or not:"))
 if (number<=1):
-    print(f"{number} is not a Prime Number")
+    
+    isPrime =False
 elif(number==2):
-    print(f"{number} is Prime Number")
+    isPrime =True
 else:
     for i in range(2, number,1):
         if (number%i==0):
-            print(f"{number} is  not a Prime Number")
+            isPrime =False
             break
-        elif(number%i!=0):
-            print(f"{number} is Prime Number")
-            break
+
+if isPrime:
+    print(f"{number} is Prime Number")
+else:
+    print(f"{number} is not a Prime Number")
 
 
 
