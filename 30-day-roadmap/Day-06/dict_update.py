892044f0ed1eac8dc5b2debefe7afb555d7f79d3
Purno <purnorunjunshqul1@gmail.com>
2026-05-30 08:53:39 +0600

    Dictionary Update

---
+++ 30-day-roadmap/Day-06/dict_update.py
@@ -0,0 +1,15 @@
+my_dict = {
+    "name": "Purno Runjun Shqul",
+    "age":23,
+    "address": "Kumarkhali Kushtia",
+    "phone":"01888534971",
+    
+
+}
+
+my_dict.update(
+    {
+        "hobby": "Electronics, Listening Muscis"
+    }
+)
+print(my_dict)
