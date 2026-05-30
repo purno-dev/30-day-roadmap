fe87c6d56b4feb66bdb6cce80eb9ef6a23b7fce4
Purno <purnorunjunshqul1@gmail.com>
2026-05-30 14:39:05 +0600

    Word Frequency Counter

--- 30-day-roadmap/Day-06/wordFrequency.py
+++ 30-day-roadmap/Day-06/wordFrequency.py
@@ -1,10 +1,11 @@
 sentence = "The Quick Brown Fox Jumps Over The Lazy Dog"
 words=sentence.split()
 word_frequency = {}
-word = 0
+word_count=0
 for word in words:
-    if word in word_frequency:
-        word+=1
+    if (word in word_frequency):
+        word_count+=1
     else:
-        word=1
+        word_count=1
+    word_frequency[word]=word_count
 print(word_frequency)
